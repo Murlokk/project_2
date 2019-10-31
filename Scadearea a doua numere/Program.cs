84c257c975dd59f01be1ba16c9b1f30d7b849911
Alexa Ciprian <alexaciprian302@yahoo.ro>
2019-10-31 14:39:15 +0200

    Scaderea a doua numere Alicatia cere introducerea fiecarui numar in parte dupa care face operatia de scadere si afiseaza rezultatul

---
+++ Scadearea a doua numere/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Test
+{
+    static void Main()
+    {
+        Console.Write("Number 1: ");
+        string stringNumber1 = Console.ReadLine();
+        int intergerNumber1 = int.Parse(stringNumber1);
+
+        Console.Write("Number 2: ");
+        string stringNumber2 = Console.ReadLine();
+        int intergerNumber2 = int.Parse(stringNumber2);
+
+        int intergerAnswer = intergerNumber1 - intergerNumber2;
+
+        Console.Write("\nYour answer is: " + intergerAnswer);
+
+        Console.Write("\n\nPes any key to exit... ");
+        Console.ReadKey();
+
+    }
+}
+
